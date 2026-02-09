fixed rotation, bigger workspace

SACEnv.py
@@ -246,9 +246,9 @@ class KitchenSACOnlineEnv(MujocoEnv):
 
         # Workspace bounds for denormalization: x: [-1.5, 0], y: [-2.5, 0], z: [1.5, 3]
         self.workspace_bounds = {
-            "x": np.array([-1.5, 0.0]),
-            "y": np.array([-2.5, 0.0]),
-            "z": np.array([1.5, 3.0]),
+            "x": np.array([-1.5, 0.3]),
+            "y": np.array([-2.3, 0.3]),
+            "z": np.array([1.0, 2.5]),
         }
 
         # Helper method to normalize position to [-1, 1] using workspace bounds
@@ -782,9 +782,10 @@ class KitchenSACOnlineEnv(MujocoEnv):
             ]
         )
 
-        target_quat = self._action_rotations_to_quaternion(
-            current[4] + (delta_rot * 0.1)
-        )
+        # target_quat = self._action_rotations_to_quaternion(
+        #    current[4] + (delta_rot * 0.1)
+        # )
+        target_quat = self._action_rotations_to_quaternion(0)
 
         # Solve IK to get target joint positions (7 arm joints)
         joint_indices = np.arange(7)  # 7 arm joints

train_SAC.py
@@ -103,7 +103,7 @@ def evaluate_agent(
 
             # Check success condition (env specific, usually term=True or info['success'])
             if term or trunc:
-                if term or reward > 6.0:
+                if term or reward > 0.6:
                     is_success = True
                 break
 
@@ -261,7 +261,7 @@ def main(args):
                 f"exploration/{k}": np.mean(v) for k, v in flatten(info).items()
             }
 
-            if reward > 5.0:
+            if reward > 0.5:
                 video_filename = f"train_ep_{episode_idx}_success_step_{step}.mp4"
                 video_path = os.path.join(save_dir, video_filename)
 
@@ -281,7 +281,7 @@ def main(args):
         if replay_buffer.size < args.seed_steps:
             continue
 
-        if step % 2 == 0:  # Ogbench does every 4
+        if step % 4 == 0:  # Ogbench does every 4
             batch = replay_buffer.sample(cfg["batch_size"])
             agent, update_info = agent.update(batch)
 